3bebacdbfa810eb5339d1bde73395601b1f5dc13
eon0111 <noe.reinosa@hotmail.com>
2024-02-13 12:58:29 +0100

    Anhadido scrollbar en actividad principal y pruebas con los sensores
    - Me apetecía explorar un poquito los layouts y he dejado el listado de sensores en la actividad principal, dentro de una vista deslizante.
    - No funciona el código para leer los valores de los sensores (he probado con el de proximidad y el de aceleración). Se inicializa todo bien, pero no se lee nada del array de valores.

---
+++ app/src/main/java/com/example/centrocivico/ActivityPanel.java
@@ -0,0 +1,47 @@
+package com.example.centrocivico;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class ActivityPanel extends AppCompatActivity implements SensorEventListener {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+		Sensor sensorProximidad = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+
+		if(sensorProximidad != null) {
+			sensorManager.registerListener((SensorEventListener) this, sensorProximidad,
+					SensorManager.SENSOR_DELAY_UI);
+		}
+	}
+
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+		if(event.values[0] == 0) {
+			visualiza("cerca");
+		} else{
+			visualiza("lejos");
+		}
+	}
+
+	public void visualiza(String val) {
+		TextView t = findViewById(R.id.valorSensorProximidad);
+		t.setText(val);
+	}
+
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+	}
+}
--- app/src/main/java/com/example/centrocivico/MainActivity.java
+++ app/src/main/java/com/example/centrocivico/MainActivity.java
@@ -15,42 +15,40 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener { //, SensorEventListener {
 
-    /* Botones */
-    private Button botonMonitorizarCaidas;
-    private Button botonMonitorizarPanel;
+	/* Botones */
+	private Button botonMonitorizarCaidas;
+	private Button botonMonitorizarPanel;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-        botonMonitorizarCaidas = (Button) findViewById(R.id.botonMonitorizarCaidas);
-        botonMonitorizarPanel = (Button) findViewById(R.id.botonMonitorizarPanel);
+		botonMonitorizarCaidas = (Button) findViewById(R.id.botonMonitorizarCaidas);
+		botonMonitorizarPanel = (Button) findViewById(R.id.botonMonitorizarPanel);
 
-        botonMonitorizarCaidas.setOnClickListener(this);
-        botonMonitorizarPanel.setOnClickListener(this);
+		botonMonitorizarCaidas.setOnClickListener(this);
+		botonMonitorizarPanel.setOnClickListener(this);
 
-        /*
-        SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        List<Sensor> listaSensores = sensorManager.getSensorList(Sensor.TYPE_ALL);
+		SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+		List<Sensor> listaSensores = sensorManager.getSensorList(Sensor.TYPE_ALL);
 
-        for(Sensor sensor: listaSensores) {
-            visualiza(sensor.getName());
-        }
-        */
-    }
+		for(Sensor sensor: listaSensores) {
+			visualiza(sensor.getName());
+		}
+	}
 
-    @Override
-    public void onClick(View v) {
-        if (v == botonMonitorizarPanel) {
-            setContentView(R.layout.activity_panel);
-        } else if (v == botonMonitorizarCaidas) {
-            setContentView(R.layout.activity_caidas);
-        }
-    }
+	@Override
+	public void onClick(View v) {
+		if (v == botonMonitorizarPanel) {
+			setContentView(R.layout.activity_panel);
+		} else if (v == botonMonitorizarCaidas) {
+			setContentView(R.layout.activity_caidas);
+		}
+	}
 
-    public void visualiza(String texto) {
-        TextView t = findViewById(R.id.cuadroTexto);
-        t.setText(t.getText() + "\n" + texto);
-    }
+	public void visualiza(String texto) {
+		TextView t = findViewById(R.id.cuadroTexto);
+		t.setText((t.getText() == "") ? texto : t.getText() + "\n" + texto);
+	}
 }
