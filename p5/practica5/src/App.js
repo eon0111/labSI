e8378a096cf1da1b7be84a851a870843ff475274
pdm858 <paauulaadoomiingueezz@gmail.com>
2024-05-10 19:22:11 +0200

    Avances en las listas
    Las listas se muestran correctamente, las fotos no se muestran, se muestra el alt en su lugar

--- p5/practica5/src/App.js
+++ p5/practica5/src/App.js
@@ -1,64 +1,57 @@
-import logo from './logo.svg';
+import fotoHennessy from './res/hennessy.jpg';
+import fotoPatterson from './res/patterson.jpg';
 import './App.css';
+import Autor from './Autor';
 
 const hennesy = {
   nombre: 'John L. Hennessy',
-  foto: './res/hennessy.jpg',
+  foto: {fotoHennessy},
   nacimiento: '22 de septiembre de 1952 en Huntington, Nueva York, EE. UU.',
-  educacion:  'Licenciatura en Ingeniería Eléctrica, 1973 - Universidad Villanova.\n'+
-              'Maestría en Ciencias en Ingeniería Eléctrica, 1975 - Universidad de Stanford.\n'+
-              'Doctorado en Ciencias en Ingeniería Eléctrica, 1977 - Universidad de Stanford.\n',
-  carrera:  'Profesor en la Universidad de Stanford, desde 1977.\n'+
-            'Presidente de la Universidad de Stanford, 2000-2016.\n',
-  contribuciones: 'Desarrollo de la arquitectura RISC (Reduced Instruction Set Computer) junto a David Patterson.\n'+
-                  'Investigación en diseño de microprocesadores y sistemas informáticos.\n',
-  premios:  'Premio Turing en 2017 por sus contribuciones a la arquitectura de computadoras, compartido con David Patterson.\n'+
-            'Miembro de la Academia Nacional de Ingeniería y de la Academia Nacional de Ciencias de EE. UU.\n',
-  bibliografia: '"Computer Architecture: A Quantitative Approach" (con David A. Patterson).\n'+
-                '"Computer Organization and Design: The Hardware/Software Interface" (con David A. Patterson).\n'+
-                '"The MIPS-X RISC Microprocessor" (con David A. Patterson y Norman Jouppi).\n'+
-                '"Computer Architecture: Concepts and Case Studies" (con David A. Patterson).\n'+
-                '"Computer Architecture: Pipelined and Parallel Processor Design" (con David A. Patterson).\n'
+  educacion:  ['Licenciatura en Ingeniería Eléctrica, 1973 - Universidad Villanova.',
+              'Maestría en Ciencias en Ingeniería Eléctrica, 1975 - Universidad de Stanford.',
+              'Doctorado en Ciencias en Ingeniería Eléctrica, 1977 - Universidad de Stanford.'],
+  carrera:  ['Profesor en la Universidad de Stanford, desde 1977.',
+            'Presidente de la Universidad de Stanford, 2000-2016.'],
+  contribuciones: ['Desarrollo de la arquitectura RISC (Reduced Instruction Set Computer) junto a David Patterson.',
+                  'Investigación en diseño de microprocesadores y sistemas informáticos.'],
+  premios:  ['Premio Turing en 2017 por sus contribuciones a la arquitectura de computadoras, compartido con David Patterson.',
+            'Miembro de la Academia Nacional de Ingeniería y de la Academia Nacional de Ciencias de EE. UU.'],
+  bibliografia: ['"Computer Architecture: A Quantitative Approach" (con David A. Patterson).',
+                '"Computer Organization and Design: The Hardware/Software Interface" (con David A. Patterson).',
+                '"The MIPS-X RISC Microprocessor" (con David A. Patterson y Norman Jouppi).',
+                '"Computer Architecture: Concepts and Case Studies" (con David A. Patterson).',
+                '"Computer Architecture: Pipelined and Parallel Processor Design" (con David A. Patterson).']
 }
 
 const patterson = {
   nombre: 'David Patterson',
-  foto: './res/patterson.jpg',
+  foto: {fotoPatterson},
   nacimiento: '16 de noviembre de 1947 en Evergreen Park, Illinois, EE. UU.',
-  educacion:  'Licenciatura en Ciencias en Física, 1969 - Universidad de California, Los Ángeles.\n'+
-              'Maestría en Ciencias en Informática, 1977 - Universidad de California, Berkeley.\n'+
-              'Doctorado en Informática, 1977 - Universidad de California, Berkeley.\n',
-  carrera:  'Profesor en la Universidad de California, Berkeley, desde 1977.\n'+
-            'Decano de la Facultad de Informática de Berkeley.\n',
-  contribuciones: 'Desarrollo de la arquitectura RISC (Reduced Instruction Set Computer).\n'+
-                  'Investigación en memoria caché y sistemas informáticos distribuidos.\n',
-  premios:  'Premio Turing en 2017 por sus contribuciones a la arquitectura de computadoras.\n'+
-            'Otros premios y reconocimientos en la comunidad informática.\n',
-  bibliografia: '"Computer Architecture: A Quantitative Approach" (con John L. Hennessy).\n'+
-                '"Computer Organization and Design: The Hardware/Software Interface" (con John L. Hennessy).\n'+
-                '"Quantitative Research for the Behavioral Sciences: A Book of Readings" (con Robert B. Zajonc y George A. Miller).\n'+
-                '"The Datacenter as a Computer: An Introduction to the Design of Warehouse-Scale Machines" (con Luiz André Barroso y Urs Hölzle).\n'+
-                '"Structured Computer Organization" (con Andrew S. Tanenbaum).\n'
+  educacion:  ['Licenciatura en Ciencias en Física, 1969 - Universidad de California, Los Ángeles.',
+              'Maestría en Ciencias en Informática, 1977 - Universidad de California, Berkeley.',
+              'Doctorado en Informática, 1977 - Universidad de California, Berkeley.'],
+  carrera:  ['Profesor en la Universidad de California, Berkeley, desde 1977.',
+            'Decano de la Facultad de Informática de Berkeley.'],
+  contribuciones: ['Desarrollo de la arquitectura RISC (Reduced Instruction Set Computer).',
+                  'Investigación en memoria caché y sistemas informáticos distribuidos.'],
+  premios:  ['Premio Turing en 2017 por sus contribuciones a la arquitectura de computadoras.',
+            'Otros premios y reconocimientos en la comunidad informática.'],
+  bibliografia: ['"Computer Architecture: A Quantitative Approach" (con John L. Hennessy).',
+                '"Computer Organization and Design: The Hardware/Software Interface" (con John L. Hennessy).',
+                '"Quantitative Research for the Behavioral Sciences: A Book of Readings" (con Robert B. Zajonc y George A. Miller).',
+                '"The Datacenter as a Computer: An Introduction to the Design of Warehouse-Scale Machines" (con Luiz André Barroso y Urs Hölzle).',
+                '"Structured Computer Organization" (con Andrew S. Tanenbaum).']
 }
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Autores de la práctica</h1>
+      <Autor person={{nombre: hennesy.nombre, foto: hennesy.foto, nacimiento: hennesy.nacimiento, educacion: hennesy.educacion, 
+        carrera: hennesy.carrera, contribuciones: hennesy.contribuciones, premios: hennesy.premios, bibliografia: hennesy.bibliografia}}/>
+      <Autor person={{nombre: patterson.nombre, foto: patterson.foto, nacimiento: patterson.nacimiento, educacion: patterson.educacion,
+        carrera: patterson.carrera, contribuciones: patterson.contribuciones, premios: patterson.premios, bibliografia: patterson.bibliografia}}/>
+    </>
   );
 }
 
--- p5/practica5/src/Autor.js
+++ p5/practica5/src/Autor.js
@@ -1,2 +1,43 @@
+function Imagen ({person}) {
+    return (
+        <img
+            className="avatar"
+            src={person.foto}
+            alt={'Foto de ' + person.nombre}
+            style={{
+              width: 90,
+              height: 90
+            }}
+        />
+    );
+}
 
+function Lista ({person}) {
+    var educacion = person.educacion.map(edu => <li className="item"> {edu} </li>);
+    var carrera = person.carrera.map(car => <li className="item"> {car} </li>);
+    var contribuciones = person.contribuciones.map(cont => <li className="item"> {cont} </li>);
+    var premios = person.premios.map(prem => <li className="item"> {prem} </li>);
+    var bibliografia = person.bibliografia.map(bib => <li className="item"> {bib} </li>);
+    return (
+        <>
+        <ul> {person.nacimiento} </ul>
+        <ul> {educacion} </ul>
+        <ul> {carrera} </ul>
+        <ul> {contribuciones} </ul>
+        <ul> {premios} </ul>
+        <ul> {bibliografia} </ul>
+        </>
+    );
+}
 
+export default function Autor ({person}) {
+    return (
+        <>
+            <h2> {person.nombre} </h2>
+            <Imagen person={{nombre: person.nombre, foto: person.foto, nacimiento: person.nacimiento, educacion: person.educacion, 
+                carrera: person.carrera, contribuciones: person.contribuciones, premios: person.premios, bibliografia: person.bibliografia}}/>
+            <Lista person={{nombre: person.nombre, foto: person.foto, nacimiento: person.nacimiento, educacion: person.educacion, 
+                carrera: person.carrera, contribuciones: person.contribuciones, premios: person.premios, bibliografia: person.bibliografia}}/>
+        </>
+    );
+}
